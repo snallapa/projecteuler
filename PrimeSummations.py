f75bb7a2ce78ed9ee244d3b7f2faca6a432ec097
Sahith Nallapareddy <sahith.reddy@gmail.com>
2018-05-25 11:54:43 -0400

    finished another one

--- PrimeSummations.py
+++ PrimeSummations.py
@@ -1,28 +1,46 @@
+import time
 primeSummations = [0,0,1,1]
 
 primeDictionary = [True for i in range(0,100000)]
 primeDictionary[1] = False
 for i in range(2,316):
-	for k in range(i*2,100000,i):
-		primeDictionary[k] = False
+    for k in range(i*2,100000,i):
+        primeDictionary[k] = False
 
 def primeList(upperBound, lowerbound=2):
     return [x for x in range(lowerbound,upperBound) if primeDictionary[x]]
 print(primeList(5))
-number = 4
+
+primes = primeList(80000)
+def sort(s):
+    return ''.join(sorted(s))
+
+start = time.time()
+combs = {}
+combs[0] = [()]
+def primeSums(n):
+    if combs.get(n, None) != None:
+        return combs[n]
+    current = []
+    for prime in primes:
+        if prime > n:
+            break
+        left = n - prime
+        possibles = primeSums(left)
+        for possible in possibles:
+            newp = list(possible) + [prime]
+            newp = tuple(sorted(newp))
+            current.append(newp)
+    current = list(set(current))
+    combs[n] = current
+    return current
+
+#print(primeSums(26))
+n = 10
 while True:
-	numberOfWays = 0
-	numbersChecked = {}
-	for prime in primeList(number):
-		numberToCheck = number - prime
-		if numbersChecked.get(prime, False) or not primeDictionary[numberToCheck]:
-			continue
-		numberOfWays = numberOfWays + primeSummations[numberToCheck]
-		numbersChecked[numberToCheck] = True
-	if primeDictionary[number]:
-		numberOfWays = numberOfWays + 1
-	primeSummations.append(numberOfWays)
-	number = number + 1
-	print("on number: {} and numberOfWays was {}".format(number, numberOfWays))
-	if numberOfWays > 5000:
-		break
+    if len(primeSums(n)) > 5000:
+        print(n)
+        end = time.time()
+        print((end - start) * 1000)
+        break
+    n += 1
